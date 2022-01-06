c39585972b30b6c5b9bdfc3d0faf139e312bd18d
David-prog-wed <dedcelisg@hotmail.es>
2022-01-06 09:08:47 -0500

    mystore

---
+++ index.js
@@ -0,0 +1,42 @@
+const express = require('express');
+const cors = require('cors');
+const routerApi = require('./routers/index');
+
+const { logErrors, errorHandler, boomErrorHandler } = require('./middlewares/error.handler');
+
+const app = express();
+const port = 3000;
+app.use(express.json());
+
+const whitelist = ['http://localhost:4000/', 'https://127.0.0.1:5500/'];
+const options = {
+  origin: (origin, callback) => {
+    if (whitelist.includes(origin) !== 1) {
+      callback(null, true);
+    } else {
+      callback(new Error('no permitido'));
+    }
+  }
+};
+
+app.use(cors(options));
+
+
+app.get('/', (req, res) => {
+  res.send('Hola mi server en Express');
+});
+
+app.get('/nueva-ruta', (req, res) => {
+  res.send('Hola, soy una nueva ruta en Express');
+});
+
+routerApi(app);
+
+app.use(logErrors);
+app.use(boomErrorHandler);
+app.use(errorHandler);
+
+
+app.listen(port, () => {
+  console.log('My port: ' + port);
+});
