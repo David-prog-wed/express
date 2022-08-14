e05713f075f39ad5b51fbb01040d8e46a2e2ec8f
David-prog-wed <dedcelisg@hotmail.es>
2022-08-14 11:56:02 -0500

    Indentando index.js con prettier

--- index.js
+++ index.js
@@ -2,7 +2,11 @@ const express = require('express');
 const cors = require('cors');
 const routerApi = require('./routers/index');
 
-const { logErrors, errorHandler, boomErrorHandler } = require('./middlewares/error.handler');
+const {
+  logErrors,
+  errorHandler,
+  boomErrorHandler,
+} = require('./middlewares/error.handler');
 
 const app = express();
 const port = process.env.PORT || 3000;
@@ -16,11 +20,10 @@ const options = {
     } else {
       callback(new Error('no permitido'));
     }
-  }
+  },
 };
 
 app.use(cors(options));
-
 
 app.get('/', (req, res) => {
   res.send('Hola mi server en Express');
@@ -35,7 +38,6 @@ routerApi(app);
 app.use(logErrors);
 app.use(boomErrorHandler);
 app.use(errorHandler);
-
 
 app.listen(port, () => {
   console.log('My port: ' + port);
